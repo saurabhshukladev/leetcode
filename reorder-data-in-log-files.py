e1831fcae523da1f2f4965e12e40f9e8ccac4fe4
shuklasaur <ankitshukla2024@gmail.com>
2024-12-16 16:11:15 +0530

    reorder-data-in-log-files solution1

---
+++ reorder-data-in-log-files.py
@@ -0,0 +1,50 @@
+# TODO implement better solution
+# TODO call the method using leetcode design
+# TODO add Readme notes
+
+def sol1(nums) -> list:
+    digitLog = []
+    letterLogMap = {}
+    tmpStr = ""
+    duplicateKeys = []
+    res = []
+    for n in nums:
+        if isDigit(n):
+            digitLog.append(n)
+        else:
+            if " ".join(n.split(" ")[1:]) in duplicateKeys:
+                letterLogMap[" ".join(n.split(" ")[1:]) + " " + n.split(" ")[0]] = n
+            
+            elif " ".join(n.split(" ")[1:]) in letterLogMap:
+                duplicateKeys.append(" ".join(n.split(" ")[1:]))
+                letterLogMap[" ".join(n.split(" ")[1:]) + " " + n.split(" ")[0]] = n
+                tmpStr = letterLogMap.pop(" ".join(n.split(" ")[1:]))
+                letterLogMap[
+                    " ".join(tmpStr.split(" ")[1:]) + " " + tmpStr.split(" ")[0]
+                ] = tmpStr
+
+            else:
+                letterLogMap[" ".join(n.split(" ")[1:])] = n
+
+    letterLogKeys = sorted(list(letterLogMap.keys()))
+    print(letterLogMap)
+    for k in letterLogKeys:
+        res.append(letterLogMap.get(k))
+
+    res.extend(digitLog)
+    return res
+
+
+def isDigit(str) -> bool:
+    flag = True
+    for s in str.split(" ")[1:]:
+        if not s.isnumeric():
+            flag = False
+
+    return flag
+
+
+nums = ["zoey i love you", "lucas i love you", "rong i love you"]
+
+print(nums)
+print(sol1(nums))
