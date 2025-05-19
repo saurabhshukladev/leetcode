1b4ac86055bf4933a049168b6bc41288f20dcece
saurabhshukladev <saurabhshukla9712@gmail.com>
2025-05-19 16:36:31 +0530

    commit just for syncing

---
+++ find-the-index-of-the-first-occurrence-in-a-string.py
@@ -0,0 +1,12 @@
+# TODO
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        print(haystack)
+        print(needle)
+        pass
+
+
+haystack = "sadasd"
+needle = "sad"
+solObj = Solution()
+print(solObj.strStr(haystack, needle))
